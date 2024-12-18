08befd891d782b86175e2767ee815943e2ab9350
AlouiOmar97 <omar.aloui@esprit.tn>
2024-12-18 16:00:02 +0100

    socket

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express')
 var app= express()
 var userRouter = require('./controller/userController')
+var chatRouter = require('./controller/chatController')
+var osRouter = require('./controller/osController')
+var productRouter = require('./controller/productController')
+var { socketIO } = require('./service/chatService')
 var mongoose = require('mongoose')
+var path = require('path')
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'twig');
 app.use(express.json())
 mongoose.connect('mongodb://localhost:27017/user-db')
         .then(()=>{
@@ -11,8 +19,13 @@ mongoose.connect('mongodb://localhost:27017/user-db')
             console.log("error : "+ error);
         })
 app.use('/users', userRouter)
+app.use('/os', osRouter)
+app.use('/chats', chatRouter)
+app.use('/product', productRouter)
+
 var http = require('http')
 var server = http.createServer(app)
+const io = socketIO(server);
 server.listen(3000,()=>{
     console.log('server started !');
 })
--- service/userService.js
+++ service/userService.js
@@ -30,5 +30,25 @@ const create =async (req,res,next)=>{
       })
 res.json('User added ! nom : '+ nom + ' email : '+ email+ ' age : '+ age)
 }
+async function updateUser(req,res,next){
+  User.findByIdAndUpdate(req.params.id,req.body)
+  .then((data,err)=>{
+    if(err){res.status(500).json(err)}
+    res.status(200).json(data)
+  })
+}
+  const deleteUser =async(req,res,next)=>{
+    try{
+    const id =req.params.id
+    const data =await User.findByIdAndDelete(id,req.body)
+    if(!data){
+      res.status(404).json("not found ... !")
+    }
+    
+    res.status(203).json(data)
+    }catch(err){
+      res.status(500).json("error : "+err)
+    }
+  }
 
-module.exports = { create, list }
+module.exports = { create, list,updateUser, deleteUser }
